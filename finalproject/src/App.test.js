85af8e188d79f1edc4987681f405b1a717908e85
Brevin <bwittmer@bsu.edu>
2018-10-20 17:20:15 -0400

    Commit to be able to pull

--- finalproject/src/App.test.js
+++ finalproject/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ClassSkills from '.ClassSkills.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+if(dice <= 0 || dice >= 7) {
+  console.log("ERROR IN DICE ROLLING");
+}
