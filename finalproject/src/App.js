ec2e99190471b08c065ae4fbdd24589ffbfa6a94
nvmanis <nvmanis@bsu.edu>
2018-11-05 14:10:50 -0500

    changed function name so project would compile - NM

--- finalproject/src/App.js
+++ finalproject/src/App.js
@@ -212,7 +212,7 @@ class App extends Component {
     return (
         <div className="App">
 
-            {this.basePoints()}
+            {this.baseHitPoints()}
             {this.printable()}
 
             {/*/!*Uncomment The below line to enable testing*!/*/}
