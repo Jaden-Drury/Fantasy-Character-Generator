e222ebb9bc411ca3bfed824f64e66bc6f5a58f4f
Jaden Drury <theamericanyank@gmail.com>
2018-11-06 20:49:11 -0500

    d10 and level 1-20 hp generation for Fighter added

--- finalproject/src/Printable.js
+++ finalproject/src/Printable.js
@@ -73,6 +73,20 @@ class Printable extends Component{
         return(sum + this.abilityModifiers(this.props.constitution)*diceNumber);
     }
 
+    rollD10(diceNumber){
+        const min = 1;
+        const max = 10;
+        var sum = 0;
+        var random = 0;
+        for(var i=diceNumber;i>0;i--){
+            random = Math.ceil(Math.random() * max-min + min);
+            console.log("random is " + random)
+            sum = sum + random;
+        }
+        return(sum + this.abilityModifiers(this.props.constitution)*diceNumber);
+    }
+
+
     leveledHP(){
         if(this.props.userClass === "Barbarian" && this.props.level>1){
             const counter = this.props.level;
@@ -92,6 +106,11 @@ class Printable extends Component{
         else if(this.props.userClass === "Druid" && this.props.level>1){
             const counter = this.props.level;
             const roll = this.rollD8(this.props.level-1)
+            return(roll);
+        }
+        else if(this.props.userClass === "Fighter" && this.props.level>1){
+            const counter = this.props.level;
+            const roll = this.rollD10(this.props.level-1)
             return(roll);
         }
         else{
